66fae974878cc19ff8342fc2bf4ef842e211df25
Natália Rodrigues <nataliamrq@gmail.com>
2020-11-18 23:31:50 -0300

    Adding .gitignore

--- .nuxt/middleware.js
+++
@@ -1,9 +0,0 @@
-const middleware = {}
-
-middleware['check-auth'] = require('../middleware/check-auth.js');
-middleware['check-auth'] = middleware['check-auth'].default || middleware['check-auth']
-
-middleware['isLogged'] = require('../middleware/isLogged.js');
-middleware['isLogged'] = middleware['isLogged'].default || middleware['isLogged']
-
-export default middleware
